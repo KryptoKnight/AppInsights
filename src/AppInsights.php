162c013bb1c71452f61246045e7e31b5a90f9961
Mark Dalby <mark@dalbymail.com>
2023-10-02 08:44:14 +0200

    test

---
+++ src/AppInsights.php
@@ -0,0 +1,12 @@
+<?php
+namespace kryptoknight\AppInsights;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * SecurityHeadersBundle
+ */
+class SecurityHeadersBundle extends Bundle
+{
+
+}
